df84f48fe9f9ccd97d9acabb184915c59057c321
robinhughesofgem <robin.hughes@ofgem.gov.uk>
2023-10-05 14:40:03 +0100

    Initial commit

---
+++ SpecFlow_BrowserStack/resources/src/SampleLocalTest.cs
@@ -0,0 +1,37 @@
+﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using SeleniumExtras.WaitHelpers;
+using System.Threading;
+
+namespace SpecFlowBrowserStack
+{
+	[Binding]
+	public class SampleLocalTest
+	{
+		private readonly IWebDriver _driver;
+
+		public SampleLocalTest()
+		{
+			_driver = BrowserStackSpecFlowTest.ThreadLocalDriver.Value;
+			new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+		}
+
+		[Given(@"I navigate to local website")]
+		public void GivenINavigatedTowebsite()
+		{
+            string? url = TestContext.Parameters["LocalUrl"];
+
+            _driver.Navigate().GoToUrl(url);
+		}
+
+		[Then(@"title should contain (.*)")]
+		public void ThenIShouldSeeTitle(string localString)
+		{
+			Thread.Sleep(5000);
+			StringAssert.Contains(localString, _driver.Title);
+		}
+	}
+}
